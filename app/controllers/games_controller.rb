e49cac02bbdd7c0eac0bc6c4b29871d2513e46da
hankyu-yatsu <ssymsh28+Git@gmail.com>
2014-12-20 06:17:43 +0900

    ゲームアップロード機能を実装

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,8 +8,12 @@ class GamesController < ApplicationController
 
   def create
     game_params = params[:game]
+    file = game_params[:game_file]
+    original_filename = file.original_filename
+    created_filename = Time.now.strftime("%y%m%d_%H%M%S_#{original_filename}")
+    File.open("upload_files/game_files/#{created_filename}","wb"){|f| f.write(file.read)}
     #@game = Game.create(title: game_params[:title], icon: game_params[:icon], group_id: game_params[:group_id], summary: game_params[:summary], version: game_params[:version], game_file: game_params[:game_file], format: game_params[:format])
-    @game = Game.create(title: game_params[:title], icon: "dammy", group_id: "1", summary: game_params[:summary], version: game_params[:version], game_file: "dammy", format: game_params[:format])
+    @game = Game.create(title: game_params[:title], icon: "dammy", group_id: "1", summary: game_params[:summary], version: game_params[:version], game_file: "upload_files/game_files/#{created_filename}", format: game_params[:format])
     for tag_num in 1..10
       tag_form = params[:tag]["#{tag_num}"][:name]
       if tag_form.empty?
