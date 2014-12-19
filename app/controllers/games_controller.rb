4772e2e2c621f62dec39b1e2c24679ea1f4baf76
hankyu-yatsu <ssymsh28+Git@gmail.com>
2014-12-20 04:44:26 +0900

    Tagのcreateの実装。アップロードされたゲームとTagの関連付けの機能を実装

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,7 +8,20 @@ class GamesController < ApplicationController
 
   def create
     game_params = params[:game]
-    @game = Game.create(title: game_params[:title], icon: game_params[:icon], group_id: game_params[:group_id], version: game_params[:version], game_file: game_params[:game_file], format: game_params[:format], group_id: 1)
+    #@game = Game.create(title: game_params[:title], icon: game_params[:icon], group_id: game_params[:group_id], summary: game_params[:summary], version: game_params[:version], game_file: game_params[:game_file], format: game_params[:format])
+    @game = Game.create(title: game_params[:title], icon: "dammy", group_id: "1", summary: game_params[:summary], version: game_params[:version], game_file: "dammy", format: game_params[:format])
+    for tag_num in 1..10
+      tag_form = params[:tag]["#{tag_num}"][:name]
+      if tag_form.empty?
+        break
+      end
+      tags = Tag.where(name: tag_form).first
+      if tags.nil?
+        tags = Tag.create(name: tag_form)
+      end
+      @game.tags << tags
+    end
+    redirect_to game_path(@game.id)
   end
 
   def edit
